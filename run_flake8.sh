cc11def099a3eb74317348496c8fd904ec102bb9
Shudipto Sekhar Roy <roy@airtonomy.ai>
2024-01-09 11:14:03 -0600

    Update run_flake8.sh

--- run_flake8.sh
+++ run_flake8.sh
@@ -1,2 +1,4 @@
-flake8 --max-line-length=120 ./*.py
-flake8 --max-line-length=120 ./*/*.py
+max_line_length=120
+
+flake8 --max-line-length=$max_line_length ./*.py
+flake8 --max-line-length=$max_line_length ./*/*.py
