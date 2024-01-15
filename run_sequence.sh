3801f5c1b27748c6a73976304ebffb17f8f51cfb
Shudipto Sekhar Roy <roy@airtonomy.ai>
2024-01-15 11:20:55 -0600

    add sequence

---
+++ run_sequence.sh
@@ -0,0 +1,7 @@
+echo ">>> TRAINING melSeg WITH example_configs/training_config_10.yaml"
+python3 melseg_trainer.py --config example_configs/training_config_10.yaml
+
+echo "\n\n\n"
+
+echo ">>> TRAINING melSeg WITH example_configs/training_config_12.yaml"
+python3 melseg_trainer.py --config example_configs/training_config_12.yaml
