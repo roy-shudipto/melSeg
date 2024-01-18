c9ebb7bf5686d892986c5cb0f7158ee7889cb2a1
Shudipto Sekhar Roy <roy@airtonomy.ai>
2024-01-18 11:35:54 -0600

    add more sequence

--- run_sequence_3.sh
+++ run_sequence_3.sh
@@ -3,10 +3,10 @@ python3 melseg_trainer.py --config example_configs/training_config_20.yaml --cud
 
 echo "\n\n\n"
 
-echo ">>> TRAINING melSeg WITH example_configs/training_config_22.yaml"
-python3 melseg_trainer.py --config example_configs/training_config_22.yaml --cuda_id 3
+echo ">>> TRAINING melSeg WITH example_configs/training_config_18.yaml"
+python3 melseg_trainer.py --config example_configs/training_config_18.yaml --cuda_id 3
 
 echo "\n\n\n"
 
-echo ">>> TRAINING melSeg WITH example_configs/training_config_24.yaml"
-python3 melseg_trainer.py --config example_configs/training_config_24.yaml --cuda_id 3
+echo ">>> TRAINING melSeg WITH example_configs/training_config_02.yaml"
+python3 melseg_trainer.py --config example_configs/training_config_02.yaml --cuda_id 3
---
+++ run_sequence_t.sh
@@ -0,0 +1,12 @@
+echo ">>> TRAINING melSeg WITH example_configs/training_config_12.yaml"
+python3 melseg_trainer.py --config example_configs/training_config_12.yaml
+
+echo "\n\n\n"
+
+echo ">>> TRAINING melSeg WITH example_configs/training_config_22.yaml"
+python3 melseg_trainer.py --config example_configs/training_config_22.yaml
+
+echo "\n\n\n"
+
+echo ">>> TRAINING melSeg WITH example_configs/training_config_24.yaml"
+python3 melseg_trainer.py --config example_configs/training_config_24.yaml
