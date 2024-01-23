786ff7464292652f140ae990f4186e6bcb0530c7
Shudipto Sekhar Roy <roy@airtonomy.ai>
2024-01-23 01:01:42 -0600

    run low LR for more epochs

--- run_sequence_3.sh
+++ run_sequence_3.sh
@@ -1,12 +1,8 @@
 echo ">>> TRAINING melSeg WITH example_configs/training_config_20.yaml"
 python3 melseg_trainer.py --config example_configs/training_config_20.yaml --cuda_id 3
 
-echo "\n\n\n"
-
-echo ">>> TRAINING melSeg WITH example_configs/training_config_18.yaml"
-python3 melseg_trainer.py --config example_configs/training_config_18.yaml --cuda_id 3
 
 echo "\n\n\n"
 
-echo ">>> TRAINING melSeg WITH example_configs/training_config_02.yaml"
-python3 melseg_trainer.py --config example_configs/training_config_02.yaml --cuda_id 3
+echo ">>> TRAINING melSeg WITH example_configs/training_config_08.yaml"
+python3 melseg_trainer.py --config example_configs/training_config_08.yaml --cuda_id 3
---
+++ run_sequence_analysis.sh
@@ -0,0 +1,49 @@
+echo ">>> Analyzing checkpoint_20240111184521"
+python3 melseg_analyzer.py --root ../checkpoints/Analyze/checkpoint_20240111184521 --outpath ../checkpoints/Analyze/checkpoint_20240111184521/log_analysis.csv
+
+echo "\n\n\n"
+
+echo ">>> Analyzing checkpoint_20240112115043"
+python3 melseg_analyzer.py --root ../checkpoints/Analyze/checkpoint_20240112115043 --outpath ../checkpoints/Analyze/checkpoint_20240112115043/log_analysis.csv
+
+echo "\n\n\n"
+
+echo ">>> Analyzing checkpoint_20240112121031"
+python3 melseg_analyzer.py --root ../checkpoints/Analyze/checkpoint_20240112121031 --outpath ../checkpoints/Analyze/checkpoint_20240112121031/log_analysis.csv
+
+echo "\n\n\n"
+
+echo ">>> Analyzing checkpoint_20240112121558"
+python3 melseg_analyzer.py --root ../checkpoints/Analyze/checkpoint_20240112121558 --outpath ../checkpoints/Analyze/checkpoint_20240112121558/log_analysis.csv
+
+echo "\n\n\n"
+
+echo ">>> Analyzing checkpoint_20240112121753"
+python3 melseg_analyzer.py --root ../checkpoints/Analyze/checkpoint_20240112121753 --outpath ../checkpoints/Analyze/checkpoint_20240112121753/log_analysis.csv
+
+echo "\n\n\n"
+
+echo ">>> Analyzing checkpoint_20240112121944"
+python3 melseg_analyzer.py --root ../checkpoints/Analyze/checkpoint_20240112121944 --outpath ../checkpoints/Analyze/checkpoint_20240112121944/log_analysis.csv
+
+echo "\n\n\n"
+
+echo ">>> Analyzing checkpoint_20240116100846"
+python3 melseg_analyzer.py --root ../checkpoints/Analyze/checkpoint_20240116100846 --outpath ../checkpoints/Analyze/checkpoint_20240116100846/log_analysis.csv
+
+echo "\n\n\n"
+
+echo ">>> Analyzing checkpoint_20240118105039"
+python3 melseg_analyzer.py --root ../checkpoints/Analyze/checkpoint_20240118105039 --outpath ../checkpoints/Analyze/checkpoint_20240118105039/log_analysis.csv
+
+echo "\n\n\n"
+
+echo ">>> Analyzing checkpoint_20240118114636"
+python3 melseg_analyzer.py --root ../checkpoints/Analyze/checkpoint_20240118114636 --outpath ../checkpoints/Analyze/checkpoint_20240118114636/log_analysis.csv
+
+echo "\n\n\n"
+
+echo ">>> Analyzing checkpoint_20240119013543"
+python3 melseg_analyzer.py --root ../checkpoints/Analyze/checkpoint_20240119013543 --outpath ../checkpoints/Analyze/checkpoint_20240119013543/log_analysis.csv
+
+echo "\n\n\n"
